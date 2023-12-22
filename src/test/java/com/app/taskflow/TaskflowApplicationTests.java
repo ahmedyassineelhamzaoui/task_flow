3c485689369c4470b503f0111ab3f4bea0220217
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-12-22 10:28:42 +0100

    initialize a spring boot project

---
+++ src/test/java/com/app/taskflow/TaskflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.taskflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
