53d05bb40f71ce48d41875d686f71bc6a7e1d874
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-12-22 11:16:49 +0100

    add method to findUserByEmail in user repository

--- src/main/java/com/app/taskflow/models/entity/User.java
+++ src/main/java/com/app/taskflow/models/entity/User.java
@@ -36,7 +36,7 @@ public class User implements UserDetails {
 
     @Enumerated(EnumType.STRING)
     private Role role;
-    
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority(role.name()));
--- src/main/java/com/app/taskflow/repositories/UserRepository.java
+++ src/main/java/com/app/taskflow/repositories/UserRepository.java
@@ -1,4 +1,12 @@
 package com.app.taskflow.repositories;
 
-public class UserRepository {
+import com.app.taskflow.models.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
 }
