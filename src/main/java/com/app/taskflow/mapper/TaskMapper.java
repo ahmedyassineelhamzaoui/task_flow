2bd368b7780e4cf6b670b3d5fdf68f7bcea170a8
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-12-25 14:15:38 +0100

    create mapper for class

--- src/main/java/com/app/taskflow/mapper/TaskMapper.java
+++ src/main/java/com/app/taskflow/mapper/TaskMapper.java
@@ -1,23 +1,22 @@
 package com.app.taskflow.mapper;
 
-
-import com.app.taskflow.models.dto.UserDTO;
-import com.app.taskflow.models.entity.User;
+import com.app.taskflow.models.dto.TaskDTO;
+import com.app.taskflow.models.entity.Task;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.ui.ModelMap;
 
 @Component
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
-public class UserDTOConverter {
+public class TaskMapper {
 
     private final ModelMapper modelMapper;
-
-    public User mapToUser(UserDTO userDTO){
-       return modelMapper.map(userDTO,User.class);
+    public Task  mapToTask(TaskDTO taskDTO){
+        return modelMapper.map(taskDTO,Task.class);
     }
-    public UserDTO mapToDTO(User user){
-        return modelMapper.map(user,UserDTO.class);
+    public TaskDTO mapToDTO(Task task){
+        return modelMapper.map(task,TaskDTO.class);
     }
 }
