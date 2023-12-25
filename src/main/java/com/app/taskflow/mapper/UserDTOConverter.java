01291cece103512b2b5739a37233fa3f73bb8a77
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-12-25 14:11:28 +0100

    create model mapper for user

---
+++ src/main/java/com/app/taskflow/mapper/UserDTOConverter.java
@@ -0,0 +1,23 @@
+package com.app.taskflow.mapper;
+
+
+import com.app.taskflow.models.dto.UserDTO;
+import com.app.taskflow.models.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class UserDTOConverter {
+
+    private final ModelMapper modelMapper;
+
+    public User mapToUser(UserDTO userDTO){
+       return modelMapper.map(userDTO,User.class);
+    }
+    public UserDTO mapToDTO(User user){
+        return modelMapper.map(user,UserDTO.class);
+    }
+}
