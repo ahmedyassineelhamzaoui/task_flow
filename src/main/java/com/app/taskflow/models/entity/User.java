850fc2e0bf08f81bc503f49e43bebf541ef40350
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-12-25 15:57:42 +0100

    add colection of roles in user

--- src/main/java/com/app/taskflow/models/entity/User.java
+++ src/main/java/com/app/taskflow/models/entity/User.java
@@ -44,10 +44,8 @@ public class User implements UserDetails {
     @OneToMany(mappedBy = "user")
     private List<Jeton> jetons;
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
-    }
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Collection<Role> authorities;
 
 
 
