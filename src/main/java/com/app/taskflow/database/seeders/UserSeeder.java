3be72d026ebf0ede4c43a9bf7813f38e38ebae7a
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-12-28 22:01:40 +0100

    create seeder for users

--- src/main/java/com/app/taskflow/database/seeders/UserSeeder.java
+++ src/main/java/com/app/taskflow/database/seeders/UserSeeder.java
@@ -1,7 +1,27 @@
 package com.app.taskflow.database.seeders;
 
+import com.app.taskflow.models.entity.UserTable;
+import com.app.taskflow.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
+@RequiredArgsConstructor
 public class UserSeeder {
+    private final UserRepository userRepository;
+
+    public void seed() {
+     if(userRepository.count() == 0){
+         List<UserTable> users = List.of(
+
+           UserTable.builder().firstName("yassine").lastName("ahmed").email("yassine@gmail.com").password("!Password12@").enabled(true).credentialsNonExpired(true).accountNonExpired(true).accountNonLocked(true).build(),
+           UserTable.builder().firstName("mohamed").lastName("saadi").email("yassine@gmail.com").password("!Password12@").enabled(true).credentialsNonExpired(true).accountNonExpired(true).accountNonLocked(true).build(),
+           UserTable.builder().firstName("adil").lastName("hajji").email("yassine@gmail.com").password("!Password12@").enabled(true).credentialsNonExpired(true).accountNonExpired(true).accountNonLocked(true).build(),
+           UserTable.builder().firstName("bassem").lastName("amin").email("yassine@gmail.com").password("!Password12@").enabled(true).credentialsNonExpired(true).accountNonExpired(true).accountNonLocked(true).build()
+
+         );
+     }
+    }
 }
