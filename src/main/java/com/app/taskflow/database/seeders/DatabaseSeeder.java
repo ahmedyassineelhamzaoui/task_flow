bc9205b9f5f5dcd5ba1e1244a89d8c571885e96b
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-12-28 22:11:36 +0100

    add method to hash password before seeding

--- src/main/java/com/app/taskflow/database/seeders/DatabaseSeeder.java
+++ src/main/java/com/app/taskflow/database/seeders/DatabaseSeeder.java
@@ -1,5 +1,6 @@
 package com.app.taskflow.database.seeders;
 
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -7,4 +8,12 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class DatabaseSeeder {
 
+    private final UserSeeder userSeeder;
+    private final RoleSeeder roleSeeder;
+
+    @PostConstruct
+    public void init() {
+        userSeeder.seed();
+        roleSeeder.seed();
+    }
 }
--- src/main/java/com/app/taskflow/database/seeders/UserSeeder.java
+++ src/main/java/com/app/taskflow/database/seeders/UserSeeder.java
@@ -3,6 +3,7 @@ package com.app.taskflow.database.seeders;
 import com.app.taskflow.models.entity.UserTable;
 import com.app.taskflow.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -11,17 +12,17 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserSeeder {
     private final UserRepository userRepository;
-
+    private final PasswordEncoder passwordEncoder;
     public void seed() {
      if(userRepository.count() == 0){
          List<UserTable> users = List.of(
 
-           UserTable.builder().firstName("yassine").lastName("ahmed").email("yassine@gmail.com").password("!Password12@").enabled(true).credentialsNonExpired(true).accountNonExpired(true).accountNonLocked(true).build(),
-           UserTable.builder().firstName("mohamed").lastName("saadi").email("yassine@gmail.com").password("!Password12@").enabled(true).credentialsNonExpired(true).accountNonExpired(true).accountNonLocked(true).build(),
-           UserTable.builder().firstName("adil").lastName("hajji").email("yassine@gmail.com").password("!Password12@").enabled(true).credentialsNonExpired(true).accountNonExpired(true).accountNonLocked(true).build(),
-           UserTable.builder().firstName("bassem").lastName("amin").email("yassine@gmail.com").password("!Password12@").enabled(true).credentialsNonExpired(true).accountNonExpired(true).accountNonLocked(true).build()
-
+           UserTable.builder().firstName("yassine").lastName("ahmed").email("yassine@gmail.com").password(passwordEncoder.encode("!Password12@")).enabled(true).credentialsNonExpired(true).accountNonExpired(true).accountNonLocked(true).build(),
+           UserTable.builder().firstName("mohamed").lastName("saadi").email("mohamed@gmail.com").password(passwordEncoder.encode("!Password12@")).enabled(true).credentialsNonExpired(true).accountNonExpired(true).accountNonLocked(true).build(),
+           UserTable.builder().firstName("adil").lastName("hajji").email("adil@gmail.com").password(passwordEncoder.encode("!Password12@")).enabled(true).credentialsNonExpired(true).accountNonExpired(true).accountNonLocked(true).build(),
+           UserTable.builder().firstName("bassem").lastName("amin").email("bbassem@gmail.com").password(passwordEncoder.encode("!Password12@")).enabled(true).credentialsNonExpired(true).accountNonExpired(true).accountNonLocked(true).build()
          );
+         userRepository.saveAll(users);
      }
     }
 }
