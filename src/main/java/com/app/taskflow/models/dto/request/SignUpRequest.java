13514304bf1c4a7d1cfbdcf9f9c670f2489b703d
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-12-29 12:15:17 +0100

    add validation for Signup request

--- src/main/java/com/app/taskflow/models/dto/request/SignUpRequest.java
+++ src/main/java/com/app/taskflow/models/dto/request/SignUpRequest.java
@@ -1,13 +1,28 @@
 package com.app.taskflow.models.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class SignUpRequest {
+
+    @NotBlank(message = "First name is required")
+    @Size(min = 2, max = 20, message = "First name must be between 2 and 20 characters")
     private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    @Size(min = 2, max = 20, message = "Last name must be between 2 and 20 characters")
     private String lastName;
+
+    @NotBlank(message = "Email is required")
+    @Email
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, max = 30, message = "Password must be between 6 and 30 characters")
     private String password;
 }
