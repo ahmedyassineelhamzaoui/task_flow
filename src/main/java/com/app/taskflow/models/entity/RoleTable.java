bdcb155fc3c76f646a8d446bf7b9aa0fdada4f5f
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-12-29 10:48:16 +0100

    add unique constraint to role

--- src/main/java/com/app/taskflow/models/entity/RoleTable.java
+++ src/main/java/com/app/taskflow/models/entity/RoleTable.java
@@ -1,9 +1,11 @@
 package com.app.taskflow.models.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.*;
+import org.hibernate.annotations.Collate;
 import org.springframework.security.core.GrantedAuthority;
 import java.util.UUID;
 
@@ -17,5 +19,7 @@ public class RoleTable implements GrantedAuthority {
     @Id
     @GeneratedValue(generator = "uuid2")
     private UUID id;
+
+    @Column(unique = true)
     private String authority;
 }
