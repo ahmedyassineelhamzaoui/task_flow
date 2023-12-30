7411c8f95a73391a805e15d51694fe250ff4dfdc
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-12-30 22:00:13 +0100

    add validation size into tag name

--- src/main/java/com/app/taskflow/models/dto/TagDTO.java
+++ src/main/java/com/app/taskflow/models/dto/TagDTO.java
@@ -2,6 +2,7 @@ package com.app.taskflow.models.dto;
 
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.util.UUID;
@@ -16,5 +17,6 @@ public class TagDTO {
     private UUID id;
 
     @NotBlank(message = "Name is mandatory")
+    @Size(min=2,message = "tag must contains at least 2 characters")
     private String name;
 }
