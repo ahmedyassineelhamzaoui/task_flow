ed1ddad3ba3023c2a2c5f64cf85541fd3799d170
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-12-30 16:15:33 +0100

    create a service interface for tag

---
+++ src/main/java/com/app/taskflow/services/facade/TagService.java
@@ -0,0 +1,21 @@
+package com.app.taskflow.services.facade;
+
+import com.app.taskflow.models.dto.TagDTO;
+import com.app.taskflow.models.entity.Tag;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public  interface TagService {
+
+    Optional<TagDTO> getTagById(UUID tagId);
+    
+    TagDTO addTag(TagDTO tagDTO);
+
+    List<TagDTO> getAllTags();
+
+    void deleteTag(UUID tagId);
+
+    TagDTO updateTag(UUID tagId, TagDTO tagDTO);
+}
