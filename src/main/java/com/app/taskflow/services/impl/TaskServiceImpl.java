8aa3bf2ad980d4656f53c20b76f8f38244dc894c
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-12-31 14:30:01 +0100

    create a method to check if the user exist before add a new task

--- src/main/java/com/app/taskflow/services/impl/TaskServiceImpl.java
+++ src/main/java/com/app/taskflow/services/impl/TaskServiceImpl.java
@@ -1,11 +1,19 @@
 package com.app.taskflow.services.impl;
 
+import com.app.taskflow.common.exception.custom.TaskTagsSizeException;
+import com.app.taskflow.common.exception.custom.TaskTimeException;
 import com.app.taskflow.mapper.TaskMapper;
 import com.app.taskflow.models.dto.TaskDTO;
 import com.app.taskflow.repositories.TaskRepository;
+import com.app.taskflow.repositories.UserRepository;
 import com.app.taskflow.services.facade.TaskService;
+import com.app.taskflow.services.facade.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.temporal.ChronoUnit;
+import java.util.NoSuchElementException;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -13,10 +21,16 @@ public class TaskServiceImpl implements TaskService {
 
     private final TaskRepository taskRepository;
     private final TaskMapper taskMapper;
+    private final UserRepository userRepository;
     @Override
     public void addTask(TaskDTO taskDTO) {
-
+        if(!isUserExist(taskDTO.getUser().getId())){
+            throw new NoSuchElementException("User not found");
+        }
         taskRepository.save(taskMapper.toEntity(taskDTO));
 
+    }
+    public boolean isUserExist(UUID id){
+        return userRepository.existsById(id);
     }
 }
