d1b06e1494d16ccecdb349b1a42b2d9dc2bec680
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-01-01 22:05:18 +0100

    add anotation for Scheduling

--- src/main/java/com/app/taskflow/TaskflowApplication.java
+++ src/main/java/com/app/taskflow/TaskflowApplication.java
@@ -3,11 +3,13 @@ package com.app.taskflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 
 @SpringBootApplication
 @EnableGlobalMethodSecurity(prePostEnabled = true,securedEnabled = true)
+@EnableScheduling
 public class TaskflowApplication  {
 
     public static void main(String[] args) {
