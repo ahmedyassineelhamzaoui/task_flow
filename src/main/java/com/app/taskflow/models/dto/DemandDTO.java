930e9b63d3e8d41dbfda0b582eb8fb27762331c8
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-01-01 21:56:49 +0100

    add DemandDTO with necessary validation

---
+++ src/main/java/com/app/taskflow/models/dto/DemandDTO.java
@@ -0,0 +1,32 @@
+package com.app.taskflow.models.dto;
+
+import com.app.taskflow.enums.OperationType;
+import com.app.taskflow.enums.OrderStatus;
+import com.app.taskflow.models.entity.Task;
+import com.app.taskflow.models.entity.UserTable;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DemandDTO {
+
+    private UUID id;
+
+    @NotBlank(message ="operation type is required ")
+    private String operationType;
+
+    private UserTable demandBy;
+
+    @NotNull(message =" task that you want to change or delete is required")
+    private Task task;
+
+    private String status = "PENDING";
+}
