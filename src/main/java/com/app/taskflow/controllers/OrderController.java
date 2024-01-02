ad6b66b10da06a9f31af02c9b26b7d77f79a90d0
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-01-02 10:18:50 +0100

    create method to add order in controller

--- src/main/java/com/app/taskflow/controllers/OrderController.java
+++ src/main/java/com/app/taskflow/controllers/OrderController.java
@@ -1,12 +1,31 @@
 package com.app.taskflow.controllers;
 
+import com.app.taskflow.common.response.ResponseWithDetails;
+import com.app.taskflow.common.response.ResponseWithoutDetails;
+import com.app.taskflow.models.dto.DemandDTO;
 import com.app.taskflow.services.facade.OrderService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class OrderController {
     private final OrderService orderService;
-    
+    private final ResponseWithoutDetails responseWithoutDetails;
+    private final ResponseWithDetails responseWithDetails;
+
+    @PostMapping("/demand")
+    public ResponseEntity<ResponseWithoutDetails> addOrder(@RequestBody @Valid DemandDTO demandDTO)  {
+        orderService.addOrder(demandDTO);
+        responseWithoutDetails.setTimestamp(LocalDateTime.now());
+        responseWithoutDetails.setStatus("200");
+        responseWithoutDetails.setMessage("Order added successfully");
+        return ResponseEntity.ok(responseWithoutDetails);
+    }
+
 }
