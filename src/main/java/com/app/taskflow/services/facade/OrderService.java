a4c5868b24ae99774f64ddcd8e9455f7a40d91e4
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-01-02 09:05:34 +0100

    create the interface of order

---
+++ src/main/java/com/app/taskflow/services/facade/OrderService.java
@@ -0,0 +1,17 @@
+package com.app.taskflow.services.facade;
+
+import com.app.taskflow.models.dto.DemandDTO;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface OrderService {
+
+    void addOrder(DemandDTO demandDTO);
+
+    List<DemandDTO> getAllOrders();
+
+    void deleteOrderById(UUID id);
+
+    DemandDTO getOrderById(UUID id);
+}
