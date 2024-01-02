290f64f158796516265c86480f71b8bce3491939
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-01-02 10:51:52 +0100

    add operation excepetion

---
+++ src/main/java/com/app/taskflow/common/exception/custom/OperationException.java
@@ -0,0 +1,7 @@
+package com.app.taskflow.common.exception.custom;
+
+public class OperationException extends RuntimeException{
+    public OperationException(String message) {
+        super(message);
+    }
+}
