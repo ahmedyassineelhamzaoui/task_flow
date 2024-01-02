f3cf71ca488a00a0f27b01f54df65bf3597ef24f
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-01-02 10:39:05 +0100

    create the addOrder method

--- src/main/java/com/app/taskflow/services/impl/OrderServiceImpl.java
+++ src/main/java/com/app/taskflow/services/impl/OrderServiceImpl.java
@@ -1,12 +1,19 @@
 package com.app.taskflow.services.impl;
 
+import com.app.taskflow.mapper.DemandMapper;
 import com.app.taskflow.models.dto.DemandDTO;
+import com.app.taskflow.models.entity.Demand;
+import com.app.taskflow.models.entity.Task;
+import com.app.taskflow.models.entity.UserTable;
 import com.app.taskflow.repositories.OrderRepository;
+import com.app.taskflow.repositories.TaskRepository;
+import com.app.taskflow.repositories.UserRepository;
 import com.app.taskflow.services.facade.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.UUID;
 
 @Service
@@ -14,9 +21,39 @@ import java.util.UUID;
 public class OrderServiceImpl  implements OrderService {
 
     private final OrderRepository orderRepository;
+    private final UserRepository userRepository;
+    private final TaskRepository taskRepository;
+    private final DemandMapper demandMapper;
+
     @Override
     public void addOrder(DemandDTO demandDTO) {
+           Task task  = taskRepository.findById(demandDTO.getTask().getId()).orElseThrow(() -> new NoSuchElementException("task not found"));
+           UserTable user    = userRepository.findById(demandDTO.getDemandBy().getId()).orElseThrow(() -> new NoSuchElementException("user not found"));
+           if(demandDTO.getOperationType().equals("MODIFICATION")) {
+               if(user.getId() != task.getAssignedTo().getId()){
 
+               }
+               if(task.isTaskAlreadyTakeJeton()){
+
+               }
+               if(user.getModificationCredit()<1){
+
+               }
+               orderRepository.save(demandMapper.toEntity(demandDTO));
+           }else if (demandDTO.getOperationType().equals("DELETION")){
+               if(user.getId() != task.getAssignedTo().getId()){
+
+               }
+               if(task.isTaskAlreadyTakeJeton()){
+
+               }
+               if(user.getDeletionCredit()<1){
+
+               }
+               orderRepository.save(demandMapper.toEntity(demandDTO));
+           }else{
+
+           }
     }
 
     @Override
