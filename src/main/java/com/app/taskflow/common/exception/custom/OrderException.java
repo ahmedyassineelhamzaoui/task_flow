f2922bde3a42eded7c827623c5390b9ed9911245
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-01-02 10:40:24 +0100

    create the orderException

---
+++ src/main/java/com/app/taskflow/common/exception/custom/OrderException.java
@@ -0,0 +1,7 @@
+package com.app.taskflow.common.exception.custom;
+
+public class OrderException extends RuntimeException {
+    public OrderException(String message){
+        super(message);
+    }
+}
