5c4a3ea1e431bfc4710916bd76cd44732ddca422
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-01-02 16:46:01 +0100

    create the updateOrderRequest

---
+++ src/main/java/com/app/taskflow/models/dto/request/UpdateDemandRequest.java
@@ -0,0 +1,32 @@
+package com.app.taskflow.models.dto.request;
+
+import com.app.taskflow.models.dto.TaskDTO;
+import com.app.taskflow.models.dto.UserTableDTO;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateDemandRequest {
+
+    private UUID id;
+
+    private String operationType;
+
+    @NotNull(message =" when you update an order to ACCEPTED you must specify the user who will do the task")
+    private UserTableDTO assignedTo;
+
+    @NotNull(message =" you should assign to user who want to change his task with  a new task")
+    private TaskDTO newTask;
+
+    @NotNull(message =" status is required")
+    private String status;
+}
